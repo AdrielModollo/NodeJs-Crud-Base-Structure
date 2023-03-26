dcaa7bb0b195244cff6785e035c213eeddec2fa1
Adriel Modollo <adrielmodollo@outlook.com>
2023-03-25 21:03:18 -0300

    feat: use jwt in all routes

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,11 +1,11 @@
 const controller = require('../controllers/usersController');
 const router = require('express').Router();
+const authenticateJWT = require('../middlewares/auth');
 
 // CRUD Routes /users
-router.get('/', controller.getUsers); // /users
-router.get('/:userId', controller.getUser); // /users/:userId
-router.post('/', controller.createUser); // /users
-router.put('/:userId', controller.updateUser); // /users/:userId
-router.delete('/:userId', controller.deleteUser); // /users/:userId
+router.get('/', authenticateJWT, controller.getUsers); // /users
+router.get('/:userId', authenticateJWT, controller.getUser); // /users/:userId
+router.put('/:userId', authenticateJWT, controller.updateUser); // /users/:userId
+router.delete('/:userId', authenticateJWT, controller.deleteUser); // /users/:userId
 
 module.exports = router;
